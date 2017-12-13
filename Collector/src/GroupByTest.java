87ee75c8a15d2e3dc1de18e36c03d4e9a79b343a
Niket Shah <niket.shah@ishisystems.com>
2017-12-13 15:19:27 +0530

    Adding stream api

--- Collector/src/GroupByTest.java
+++ Collector/src/GroupByTest.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class GroupByTest {
@@ -13,16 +14,50 @@ public class GroupByTest {
                 new Employee("Employee5", 45, "Admin"));
 
         System.out.println("\nUsing groupingBy First Varient");
-        Map<String, List<Employee>> stringListMap = employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment));
+        Map<String, List<Employee>> stringListMap = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment));
         stringListMap.forEach((s, employees) -> System.out.println(s + " - " + employees));
 
         System.out.println("\nUsing groupingBy Second Varient");
-        Map<String, Set<Employee>> stringSetMap = employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment, Collectors.toSet()));
+        Map<String, Set<Employee>> stringSetMap = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.toSet()));
         stringSetMap.forEach((s, employees) -> System.out.println(s + " - " + employees));
 
         System.out.println("\nUsing groupingBy Third Varient");
-        TreeMap<String, Set<Employee>> treeMap = employeeList.stream().collect(Collectors.groupingBy(Employee::getDepartment, TreeMap::new, Collectors.toSet()));
+        TreeMap<String, Set<Employee>> treeMap = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, TreeMap::new, Collectors.toSet()));
         treeMap.forEach((s, employees) -> System.out.println(s + " - " + employees));
+
+        System.out.println("\nUsing groupBy & averagingDouble");
+        Map<String, Double> doubleMap = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment, Collectors.averagingInt(Employee::getAge)));
+        doubleMap.forEach((s, aDouble) -> System.out.println(s + " - " + aDouble));
+
+
+        System.out.println("\nUsing groupBy & summingLong");
+        Map<String, LongSummaryStatistics> statisticsMap = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment,
+                        Collectors.summarizingLong(Employee::getAge)));
+        statisticsMap.forEach((s, longSummaryStatistics) -> System.out.println(s + " - " + longSummaryStatistics));
+
+        System.out.println("\nUsing groupBy & mapping");
+        Map<String, List<String>> listMap = employeeList.stream()
+                .collect(Collectors.groupingBy(Employee::getDepartment,
+                        Collectors.mapping(Employee::getName, Collectors.toList())));
+        System.out.println(listMap);
+
+        System.out.println("\n---New Example--");
+
+        List<String> items =
+                Arrays.asList("apple", "apple", "banana",
+                        "apple", "orange", "banana", "papaya");
+
+        Map<String, Long> collect = items.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        System.out.println(collect);
+
+        System.out.println("Add sorting");
+        collect.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
 
     }
 }
---
+++ Collector/src/MapSort.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MapSort {
+
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(15, "Mahesh");
+        map.put(10, "Suresh");
+        map.put(30, "Nilesh");
+
+
+        map.entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+
+
+        map.entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getValue))
+                .forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+
+
+        List<String> name = new ArrayList<>();
+        map.entrySet().stream()
+                .sorted(Comparator.comparing(Map.Entry::getValue))
+                .peek(integerStringEntry -> name.add(integerStringEntry.getValue()))
+                .forEach(e -> System.out.println(e.getKey() + " - " + e.getValue()));
+        System.out.println(name);
+
+        System.out.println("Collecting in map ");
+        LinkedHashMap<Integer, String> hashMap = map.entrySet().stream()
+                .sorted(Map.Entry.comparingByValue())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (integer, integer2) -> integer, LinkedHashMap::new));
+
+        hashMap.forEach(
+                (integer, s) -> System.out.println(integer + " - " + s)
+        );
+
+    }
+}
